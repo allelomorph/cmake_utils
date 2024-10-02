c4a369024ccfae7aacbe41d6f9edfd55c7707d9f
allelomorph <samuel.pomeroy@atlasschool.com>
2024-10-02 20:40:10 +0000

    GetCatch2: reworked to use CATCH_DEFAULT_VERSION

--- GetCatch2.cmake
+++ GetCatch2.cmake
@@ -31,18 +31,27 @@ endif()
 # - test integration script Catch.cmake in `extras` dir, see:
 #   - https://github.com/catchorg/Catch2/blob/v3.4.0/docs/cmake-integration.md
 
-# See https://packages.ubuntu.com/search?keywords=catch2 for package versions
-#
-# Relevant Catch2 releases:
-# - release matching Ubuntu 22.04 LTS package:
-#   2.13.8  216713a4066b79d9803d374f261ccb30c0fb451f tag "v2.13.8"
-# - release matching Ubuntu 23.10 package:
-#   2.13.10 182c910b4b63ff587a3440e08f84f70497e49a81 tag "v2.13.10"
-# - release matching Ubuntu 24.04 LTS package:
-#   3.4.0   6e79e682b726f524310d55dec8ddac4e9c52fb5f tag "v3.4.0"
-set(CATCH_DEFAULT_VERSION "3.4.0")
+# earliest release to use multi-header implementation:
+# 3.0.1 605a34765aa5d5ecbf476b4598a862ada971b0cc
+# earliest release with robust catch_discover_tests:
+set(CATCH_MINIMUM_VERSION "3.4.0")  # 6e79e682b726f524310d55dec8ddac4e9c52fb5f
+# latest release at last script update
+#   - also introduces cmake support of skipped tests via SKIP_RETURN_CODE, see:
+#     - https://github.com/catchorg/Catch2/issues/2873
+set(CATCH_CURRENT_VERSION "3.7.1")  # fa43b77429ba76c462b1898d6cd2f2d7a9416b14
+
+if(NOT CATCH_DEFAULT_VERSION OR
+    "${CATCH_DEFAULT_VERSION}" VERSION_LESS "${CATCH_MINIMUM_VERSION}")
+  set(CATCH_TARGET_VERSION "${CATCH_MINIMUM_VERSION}")
+else()
+  set(CATCH_TARGET_VERSION "${CATCH_DEFAULT_VERSION}")
+endif()
+
 set(FP_OPTIONS
-  ${CATCH_DEFAULT_VERSION}
+  # Any newer version of the same major should be accepted, see:
+  #   - https://github.com/catchorg/Catch2/blob/v2.13.10/CMakeLists.txt#L178
+  #   - https://github.com/catchorg/Catch2/blob/v3.4.0/CMakeLists.txt#L123
+  ${CATCH_MINIMUM_VERSION}
   # cmake (through v3.30 at least) does not supply FindCatch2.cmake, and Catch2
   #   creates its own config file, see:
   #   - https://github.com/catchorg/Catch2/blob/v2.13.10/CMakeLists.txt#L194
@@ -51,25 +60,27 @@ set(FP_OPTIONS
 )
 set(FC_OPTIONS
     GIT_REPOSITORY https://github.com/catchorg/Catch2.git
-    GIT_TAG        "v${CATCH_DEFAULT_VERSION}"
+    GIT_TAG        "v${CATCH_TARGET_VERSION}"
 )
 fetch_if_not_found(Catch2 "${FP_OPTIONS}" "${FC_OPTIONS}")
 
-# _CATCH_VERSION_MAJOR intended to be set as compile definition for test targets,
-#   this allows for selective includes to accommodate differences in v2 and v3
-#   (leading underscore to prevent shadowing CATCH_VERSION_MAJOR, which may be
-#   defined by Catch header inclusions)
-if(DEFINED CATCH_VERSION_MAJOR)
-  set(_CATCH_VERSION_MAJOR "${CATCH_VERSION_MAJOR}")
-else()
+# find_package success in config mode should set <PackageName>_VERSION*, see:
+#   - https://cmake.org/cmake/help/v3.30/command/find_package.html#version-selection
+# If fetched instead similar catch2_* variables will not be set by FetchContent, see:
+#   - https://cmake.org/cmake/help/v3.30/module/FetchContent.html#command:fetchcontent_populate
+if(catch2_POPULATED)
+  set(Catch2_VERSION "${CATCH_TARGET_VERSION}")
   string(REGEX REPLACE
     "^([0-9]+)\.[0-9]+\.[0-9]+$" "\\1"
-    _CATCH_VERSION_MAJOR "${CATCH_DEFAULT_VERSION}")
+    Catch2_VERSION_MAJOR "${Catch2_VERSION}")
+  string(REGEX REPLACE
+    "^[0-9]+\.([0-9]+)\.[0-9]+$" "\\1"
+    Catch2_VERSION_MINOR "${Catch2_VERSION}")
+  string(REGEX REPLACE
+    "^[0-9]+\.[0-9]+\.([0-9]+)$" "\\1"
+    Catch2_VERSION_PATCH "${Catch2_VERSION}")
+  set(Catch2_VERSION_COUNT 3)
 endif()
-if(NOT _CATCH_VERSION_MAJOR)
-  message(FATAL_ERROR "Could not determine Catch2 required major version")
-endif()
-unset(CATCH_DEFAULT_VERSION)
 
 # adding path to Catch.cmake to allow `include(Catch)`, see above links on
 #   cmake integration
@@ -80,7 +91,7 @@ elseif(DEFINED Catch2_SOURCE_DIR)  # Catch2_FOUND
 else()
   message(FATAL_ERROR "Could not find Catch2 source directory")
 endif()
-if(_CATCH_VERSION_MAJOR EQUAL 2)
+if(${Catch2_VERSION_MAJOR} EQUAL 2)
   list(APPEND CMAKE_MODULE_PATH "${CATCH_SOURCE_DIR}/contrib")
 else()
   list(APPEND CMAKE_MODULE_PATH "${CATCH_SOURCE_DIR}/extras")
