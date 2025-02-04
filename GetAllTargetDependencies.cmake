9ba71807706dc044a1186d7e1e2ee532ecf7b605
allelomorph <samuel.pomeroy@atlasschool.com>
2025-02-04 03:39:24 +0000

    GetAllTargetDependencies: wrap recursion as impl, filter targets

--- GetAllTargetDependencies.cmake
+++ GetAllTargetDependencies.cmake
@@ -7,11 +7,37 @@ include_guard(DIRECTORY)
 #
 #   target (string): name of root target
 #   dep_list_var (string): variable in parent scope to store dependency list,
-#     cannot be "_deps" to avoid shadowing internal variable
+#     cannot be "_deps" to avoid shadowing internal implementation variable
 #
-function(get_all_target_dependencies target dep_list_var)
+macro(get_all_target_dependencies target dep_list_var)
   if(NOT TARGET ${target})
     return()
+  endif()
+
+  if(${dep_list_var} STREQUAL "_deps")
+    message(FATAL_ERROR
+      "get_all_target_dependencies(): dep_list_var cannot be \"_deps\", "
+      "a protected internal variable name")
+  endif()
+
+  set(_inside_get_all_target_dependencies YES)
+  _get_all_target_dependencies_impl(${target} ${dep_list_var})
+  unset(_inside_get_all_target_dependencies)
+  # TBD list(REMOVE_DUPLICATES ${dep_list_var}) once here after recursion
+  #   more efficient than checking if(NOT IN_LIST) for each new dep?
+  set(${dep_list_var} ${${dep_list_var}} PARENT_SCOPE)
+
+endmacro(get_all_target_dependencies)
+
+# _get_all_target_dependencies_impl(target dep_list_var)
+#   internal implementation of get_all_target_dependencies(), all arguments
+#   passed unmodified
+#
+function(_get_all_target_dependencies_impl target dep_list_var)
+  if(NOT _inside_get_all_target_dependencies)
+    message(FATAL_ERROR
+      "_get_all_target_dependencies_impl(): can only be called as part of "
+      "get_all_target_dependencies")
   endif()
 
   # build set of all dependencies of current target, PUBLIC|PRIVATE|INTERFACE
@@ -19,23 +45,19 @@ function(get_all_target_dependencies target dep_list_var)
   get_target_property(interface_link_libs ${target} INTERFACE_LINK_LIBRARIES)
   unset(_deps)  # unset needed due to recursive calls
   if(interface_link_libs)
-    set(_deps ${interface_link_libs})
+    list(APPEND _deps ${interface_link_libs})
   endif()
   if(link_libs)
-    foreach(lib ${link_libs})
-      if(NOT ${lib} IN_LIST _deps)
-        list(APPEND _deps ${lib})
-      endif()
-    endforeach()
+    list(APPEND _deps ${link_libs})
   endif()
 
   # combine current target dependency set with root target set
   foreach(dep ${_deps})
-    if(NOT ${dep} IN_LIST ${dep_list_var})
+    if(TARGET ${dep} AND NOT ${dep} IN_LIST ${dep_list_var})
       list(APPEND ${dep_list_var} ${dep})
+      _get_all_target_dependencies_impl(${dep} ${dep_list_var})
     endif()
-    get_all_target_dependencies(${dep} ${dep_list_var})
   endforeach()
   set(${dep_list_var} ${${dep_list_var}} PARENT_SCOPE)
 
-endfunction(get_all_target_dependencies)
+endfunction(_get_all_target_dependencies_impl)
