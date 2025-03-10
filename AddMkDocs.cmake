191434ef6752abc9af82c690dae0bd8ed9327422
allelomorph <samuel.pomeroy@atlasschool.com>
2025-03-10 23:29:20 +0000

    added AddMkDocs and GetDoxide plus helper templates

---
+++ AddMkDocs.cmake
@@ -0,0 +1,270 @@
+# newest features used: PROJECT_IS_TOP_LEVEL v3.21
+cmake_minimum_required(VERSION 3.21)
+
+include_guard(DIRECTORY)
+
+if(COMMAND add_mkdocs)
+  return()
+endif()
+
+##
+## Check dependencies
+##
+
+if(NOT doxide_POPULATED)
+  include(GetDoxide)
+endif()
+
+find_package(Python3)
+
+find_program(PIP_BINARY
+  pip
+)
+
+if(PIP_BINARY)
+  foreach(pip_pkg mkdocs mkdocs-material)
+    execute_process(
+      COMMAND pip show ${pip_pkg}
+      RESULT_VARIABLE ${pip_pkg}_FOUND
+      OUTPUT_QUIET
+      ERROR_QUIET
+    )
+    # bash exit codes are inverse of expected booleans
+    if(${pip_pkg}_FOUND)
+      set(${pip_pkg}_FOUND FALSE)
+    else()
+      set(${pip_pkg}_FOUND TRUE)
+    endif()
+  endforeach()
+endif()
+
+if(NOT doxide_POPULATED)
+  message(FATAL_ERROR
+    "doxide not found; required to build markdown inputs for mkdocs")
+endif()
+
+if(NOT Python3_FOUND)
+  message(FATAL_ERROR
+    "python3 not found; required for use of mkdocs")
+endif()
+
+if(NOT PIP_BINARY)
+  message(FATAL_ERROR
+    "python3 pip not found; required for use of mkdocs")
+endif()
+
+if(NOT mkdocs_FOUND)
+  message(FATAL_ERROR
+    "pip package mkdocs not found; please ensure that you are in the \
+correct python environment")
+endif()
+
+if(NOT mkdocs-material_FOUND)
+  message(FATAL_ERROR
+    "pip package mkdocs-material not found; please ensure that you are in the \
+correct python environment")
+endif()
+
+# add_mkdocs()
+#   Sets up MkDocs automated documentation generation for the project,
+#     accesible via the `mkdocs` target. Also produces Markdown documentation
+#     via the `doxide_md` target.
+#
+function(add_mkdocs)
+
+  if(TARGET mkdocs)
+    return()
+  endif()
+
+  ##
+  ## Create doxide setup script
+  ##
+
+  # doxide v0.9.0 expects file paths in doxide.yaml to be relative to the
+  #   directory in which it is run, so in order to run it in the build
+  #   directory we need to make the file paths absolute. Additionally, we
+  #   cannot know And to make them
+  #   absolute, we need PROJECT_SOURCE_DIR, which is not known until config
+  #   time.
+  set(setup_script_text [[
+# newest features used: cmake -E rm v3.17
+cmake_minimum_required(VERSION 3.17)
+
+# first init doxide to create any files not supplied by PROJECT_SOURCE_DIR
+execute_process(
+  # `doxide init` will hang waiting for user confirmation for deletion of
+  #    previous files, even when piping in `yes`
+  COMMAND ${CMAKE_COMMAND} -E rm -rf docs/ doxide.yaml mkdocs.yaml
+  COMMAND @doxide_BINARY_DIR@/doxide init
+  WORKING_DIRECTORY @PROJECT_BINARY_DIR@
+  OUTPUT_QUIET
+  COMMAND_ERROR_IS_FATAL ANY
+)
+
+# Pre-existing @PROJECT_SOURCE_DIR@/docs intended when topic/nav bar
+#   markdown pages not derived from source are part of the documentation.
+#   In that case make sure that the mkdocs.yaml has the proper `nav` section:
+#   - https://www.mkdocs.org/user-guide/configuration/#nav
+#   and that the docs dir contains the subdirectories produced by
+#   `doxide init`
+if(IS_DIRECTORY @PROJECT_SOURCE_DIR@/docs)
+  # already screened for doxide init support files in add_mkdocs()
+  file(COPY @PROJECT_SOURCE_DIR@/docs
+    DESTINATION @PROJECT_BINARY_DIR@
+  )
+endif()
+
+# doxide v0.9.0 expects file paths in doxide.yaml to be relative to the
+#   directory in which it is run, so in order to run it in the build
+#   directory we need to make the file paths absolute.
+if(EXISTS @PROJECT_SOURCE_DIR@/doxide.yaml.in)
+  # We must define again here any cmake variables that will be consumed by
+  #   doxide.yaml.in, as it will be configured at build time
+  set(PROJECT_SOURCE_DIR "@PROJECT_SOURCE_DIR@")
+  set(PROJECT_NAME "@PROJECT_NAME@")
+  set(PROJECT_DESCRIPTION "@PROJECT_DESCRIPTION@")
+  configure_file(
+    @PROJECT_SOURCE_DIR@/doxide.yaml.in
+    @PROJECT_BINARY_DIR@/doxide.yaml
+    ESCAPE_QUOTES
+  )
+elseif(EXISTS @PROJECT_SOURCE_DIR@/doxide.yaml)
+  file(COPY_FILE
+    @PROJECT_SOURCE_DIR@/doxide.yaml
+    @PROJECT_BINARY_DIR@/doxide.yaml
+  )
+else()
+  # use default from prior `doxide init`
+endif()
+
+if(EXISTS @PROJECT_SOURCE_DIR@/mkdocs.yaml.in)
+  # We must define again here any cmake variables that will be consumed by
+  #   mkdocs.yaml.in, as it will be configured at build time
+  set(PROJECT_SOURCE_DIR "@PROJECT_SOURCE_DIR@")
+  set(PROJECT_NAME "@PROJECT_NAME@")
+  set(PROJECT_DESCRIPTION "@PROJECT_DESCRIPTION@")
+  execute_process(
+    COMMAND git config --get remote.origin.url
+    WORKING_DIRECTORY @PROJECT_SOURCE_DIR@
+    OUTPUT_VARIABLE git_remote_origin_url
+    ERROR_QUIET
+  )
+  if(git_remote_origin_url)
+    string(REGEX REPLACE
+      "^(.*)\\.git$" "\\1"
+      MKDOCS_REPO_URL ${git_remote_origin_url})
+    string(REGEX REPLACE
+      "^https:\\/\\/(git(hub|lab|kraken)|bitbucket).com\\/(.*)$" "\\3"
+      MKDOCS_REPO_NAME ${MKDOCS_REPO_URL})
+    string(REGEX REPLACE
+      "^https:\\/\\/(git(hub|lab|kraken)|bitbucket).com\\/(.*)$" "\\1"
+      MKDOCS_REPO_ICON ${MKDOCS_REPO_URL})
+    set(MKDOCS_REPO_ICON "fontawesome/brands/${MKDOCS_REPO_ICON}")
+  endif()
+  set(MKDOCS_USE_DIRECTORY_URLS "false")
+  configure_file(
+    @PROJECT_SOURCE_DIR@/mkdocs.yaml.in
+    @PROJECT_BINARY_DIR@/mkdocs.yaml
+    ESCAPE_QUOTES
+  )
+elseif(EXISTS @PROJECT_SOURCE_DIR@/mkdocs.yaml)
+  file(COPY_FILE
+    @PROJECT_SOURCE_DIR@/mkdocs.yaml
+    @PROJECT_BINARY_DIR@/mkdocs.yaml
+  )
+else()
+  # use default from prior `doxide init`
+endif()
+]])
+  if(NOT IS_DIRECTORY ${PROJECT_SOURCE_DIR}/cmake)
+    file(MAKE_DIRECTORY ${PROJECT_SOURCE_DIR}/cmake)
+  endif()
+  set(setup_script_name "SetUpDoxideMkDocs.cmake")
+  set(setup_script_path ${PROJECT_SOURCE_DIR}/cmake/${setup_script_name})
+  file(WRITE ${setup_script_path}.in "${setup_script_text}")
+    configure_file(
+      ${setup_script_path}.in
+      ${setup_script_path}
+    )
+  file(REMOVE ${setup_script_path}.in)
+
+  ##
+  ## Add documentation build targets
+  ##
+
+  file(GLOB_RECURSE setup_dependencies
+    ${PROJECT_SOURCE_DIR}/docs/*.md
+    ${PROJECT_SOURCE_DIR}/docs/*.markdown
+  )
+  if(IS_DIRECTORY ${PROJECT_SOURCE_DIR}/docs)
+    set(doxide_docs_contents
+      ${PROJECT_SOURCE_DIR}/docs/javascripts/mathjax.js
+      ${PROJECT_SOURCE_DIR}/docs/javascripts/tablesort.js
+      ${PROJECT_SOURCE_DIR}/docs/overrides/partials/copyright.html
+      ${PROJECT_SOURCE_DIR}/docs/stylesheets/doxide.css
+    )
+    foreach(path ${doxide_docs_contents})
+      if(NOT EXISTS ${path})
+        message(FATAL_ERROR "add_mkdocs(): ${PROJECT_SOURCE_DIR}/docs, if \
+present, must contain all support files created by `doxide init`; could not \
+find ${path}")
+      endif()
+      list(APPEND setup_dependencies ${path})
+    endforeach()
+  endif()
+  if(EXISTS ${PROJECT_SOURCE_DIR}/doxide.yaml.in)
+    list(APPEND setup_dependencies ${PROJECT_SOURCE_DIR}/doxide.yaml.in)
+  endif()
+  if(EXISTS ${PROJECT_SOURCE_DIR}/mkdocs.yaml.in)
+    list(APPEND setup_dependencies ${PROJECT_SOURCE_DIR}/mkdocs.yaml.in)
+  endif()
+
+  add_custom_command(
+    OUTPUT
+      ${PROJECT_BINARY_DIR}/docs/javascripts/mathjax.js
+      ${PROJECT_BINARY_DIR}/docs/javascripts/tablesort.js
+      ${PROJECT_BINARY_DIR}/docs/overrides/partials/copyright.html
+      ${PROJECT_BINARY_DIR}/docs/stylesheets/doxide.css
+      ${PROJECT_BINARY_DIR}/doxide.yaml
+      ${PROJECT_BINARY_DIR}/mkdocs.yaml
+    COMMAND
+      ${CMAKE_COMMAND} -P ${setup_script_path}
+    # TBD when copying in docs/, for some reason it is considered always out of date
+    DEPENDS ${setup_dependencies}
+    WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+    COMMENT "Setting up doxide and mkdocs builds"
+    USES_TERMINAL
+    DEPENDS_EXPLICIT_ONLY
+  )
+
+  add_custom_target(doxide_md
+    doxide build
+    DEPENDS
+      doxide
+      ${PROJECT_BINARY_DIR}/docs/javascripts/mathjax.js
+      ${PROJECT_BINARY_DIR}/docs/javascripts/tablesort.js
+      ${PROJECT_BINARY_DIR}/docs/overrides/partials/copyright.html
+      ${PROJECT_BINARY_DIR}/docs/stylesheets/doxide.css
+      ${PROJECT_BINARY_DIR}/doxide.yaml
+      ${PROJECT_BINARY_DIR}/mkdocs.yaml
+    WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+    COMMENT
+      "Building Doxide markdown documentation"
+    USES_TERMINAL
+  )
+
+  message(STATUS "Added Doxide target doxide_md to build Markdown docs")
+
+  # TBD --coverage support
+  add_custom_target(mkdocs
+    mkdocs build --site-dir mkdocs_site
+    WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+    COMMENT
+      "Building MkDocs HTML documentation"
+    USES_TERMINAL
+  )
+  add_dependencies(mkdocs doxide_md)
+
+  message(STATUS "Added MkDocs target mkdocs to build HTML docs")
+
+endfunction()
---
+++ GetDoxide.cmake
@@ -0,0 +1,125 @@
+# newest feature TBD
+cmake_minimum_required(VERSION 3.20)
+
+if(doxide_POPULATED)
+  return()
+endif()
+
+if(NOT COMMAND FetchContent_Declare OR
+    NOT COMMAND FetchContent_MakeAvailable)
+  include(FetchContent)
+endif()
+
+set(patch_path ${CMAKE_CURRENT_BINARY_DIR}/patches/doxide)
+
+# Updating use of CMAKE_SOURCE_DIR to CMAKE_CURRENT_SOURCE_DIR allows it to
+#   work both as a standalone build and as a FetchContent dependency
+if(NOT EXISTS ${patch_path}/downstream_FetchContent.patch)
+  file(WRITE ${patch_path}/downstream_FetchContent.patch [[
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index 3d8a966..059d8bb 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -58,15 +58,15 @@ target_link_libraries(doxide
+ )
+ 
+ configure_file(
+-    ${CMAKE_SOURCE_DIR}/src/config.h.in
+-    ${CMAKE_SOURCE_DIR}/src/config.h
++    ${CMAKE_CURRENT_SOURCE_DIR}/src/config.h.in
++    ${CMAKE_CURRENT_SOURCE_DIR}/src/config.h
+ )
+ 
+ include(GNUInstallDirs)
+ install(TARGETS doxide RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR})
+ 
+-set(CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/win ${CMAKE_MODULE_PATH})
+-string(REPLACE "/" "\\\\" NATIVE_SOURCE_DIR "${CMAKE_SOURCE_DIR}")
++set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/win ${CMAKE_MODULE_PATH})
++string(REPLACE "/" "\\\\" NATIVE_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}")
+ set(CPACK_PACKAGE_NAME "Doxide")
+ set(CPACK_PACKAGE_ICON "${NATIVE_SOURCE_DIR}\\\\win\\\\icon.ico")
+ set(CPACK_PACKAGE_VENDOR "Lawrence Murray")
+]])
+endif()
+
+# Updating comment block parsing to ignore @brief (as a token) and @file (by
+#   skipping the entire comment block) improves cross-compatibility with Doxygen
+if(NOT EXISTS ${patch_path}/ignore_file_and_brief_cmds.patch)
+  file(WRITE ${patch_path}/ignore_file_and_brief_cmds.patch [[
+diff --git a/src/Doc.cpp b/src/Doc.cpp
+index 5cdea02..d8d1511 100644
+--- a/src/Doc.cpp
++++ b/src/Doc.cpp
+@@ -81,9 +81,11 @@ Doc::Doc(const std::string_view comment, const int init_indent) :
+           docs.append("\n:material-location-exit: **Return**\n:   ");
+         } else if (command == "sa") {
+           docs.append("\n:material-eye-outline: **See**\n:   ");
+-        } else if (command == "file" ||
+-            command == "internal") {
+-          hide = true;
++        } else if (command == "file") {
++            /* ignore entire comment (for Doxygen cross-compatibility) */
++            tokenizer.consume(CLOSE);
++        } else if (command == "internal") {
++            hide = true;
+         } else if (command == "e" ||
+             command == "em" ||
+             command == "a") {
+@@ -145,6 +147,8 @@ Doc::Doc(const std::string_view comment, const int init_indent) :
+             command == "property") {
+           /* ignore, including following name */
+           tokenizer.consume(WORD);
++        } else if (command == "brief") {
++          /* ignore token (for Doxygen cross-compatibility) */
+         } else if (command == "@") {
+           docs.append("@");
+         } else if (command == "/") {
+]])
+endif()
+
+# Doxide v0.9.0 (latest tag at time of writing) uses git submodules rather than
+#   FetchContent, perhaps due to not building any of the dependencies other
+#   than YAML (when requested.) Cloning is much quicker when using
+#   --shallow-submodules so that all repos are at a depth of 1.
+#   However, while FetchContent supports --depth 1 (GIT_SHALLOW) and
+#   --recurse-submodules (GIT_SUBMODULES_RECURSE,) it does not support
+#   --shallow-submodules:
+#   - https://gitlab.kitware.com/cmake/cmake/-/issues/16144
+#   So we must use a custom DOWNLOAD_COMMAND
+set(FETCHCONTENT_QUIET OFF)
+set(bash_prefix     "/usr/bin/bash" "-c" )
+set(git_repository  https://github.com/lawmurray/doxide.git )
+set(git_tag         v0.9.0 )
+set(fc_src_dir      ${FETCHCONTENT_BASE_DIR}/doxide-src )
+set(clone_options
+  "--branch ${git_tag} --depth 1 --recurse-submodules --shallow-submodules" )
+set(patch_paths
+  ${patch_path}/downstream_FetchContent.patch
+  ${patch_path}/ignore_file_and_brief_cmds.patch
+  ${patch_path}/init_observes_output_longopt.patch
+)
+list(JOIN patch_paths " " patch_paths)
+
+set(BUILD_YAML ON CACHE BOOL "build Doxide libyaml dependency from source")
+set(BUILD_TESTING OFF CACHE BOOL "skip building Doxide libyaml dependency tests")
+
+# Each *_COMMAND works best when internally idempotent, always exits with 0,
+#   and contains no enescaped semicolons
+FetchContent_Declare(doxide
+  DOWNLOAD_COMMAND ${bash_prefix}
+    "git -C ${fc_src_dir} pull &> /dev/null || \
+      git clone ${clone_options} ${git_repository} ${fc_src_dir}"
+  PATCH_COMMAND ${bash_prefix}
+    "git diff --quiet && git apply ${patch_paths} || true"
+  )
+FetchContent_MakeAvailable(doxide)
+
+unset(patch_path)
+set(FETCHCONTENT_QUIET ON)
+unset(bash_prefix)
+unset(git_repository)
+unset(git_tag)
+unset(fc_src_dir)
+unset(clone_options)
+unset(patch_paths)
